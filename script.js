f545f47d3a4079e421df2a7048ab588dc532a669
tempA1-UX <alamudok@gmail.com>
2026-04-15 10:59:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// TIME
+function updateTime() {
+  document.getElementById("time").textContent = Date.now();
+}
+updateTime();
+setInterval(updateTime, 1000);
+
+// AVATAR UPLOAD
+const avatar = document.getElementById("avatar");
+const upload = document.getElementById("upload");
+
+document.querySelector(".upload-btn").onclick = () => upload.click();
+
+upload.addEventListener("change", () => {
+  const file = upload.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      avatar.src = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
